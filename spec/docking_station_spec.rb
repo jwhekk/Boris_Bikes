31d714334bb86ca284fa16fd151dda3d72af0d80
Matthew Drake <matdrake@live.co.uk>
2016-01-19 16:15:55 +0000

    Added lib directory. Created docking_station.rb and defined DockingStation class within it

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
-describe DockingStation do
- 
- it { should be_kind_of(Class) }
- it { should be_a_kind_of(Class) }
- it { should be_a(Class) }
+require 'docking_station'
 
+describe DockingStation do
+ #it { should be_kind_of(Class) }
+ #it { should be_a_kind_of(Class) }
+ #it { should be_a(Class) }
 end

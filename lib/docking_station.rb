50e9499faa7a8e35d672ed2be3f89d8fc25d5572
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-20 16:43:37 +0100

    added instance variable and feature and unit test for dock

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,11 @@
 require_relative 'bike.rb'
 
 class DockingStation
+  attr_reader :bikes
   def release_bike
     Bike.new
+  end
+  def dock(bike)
+    @bikes = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
-  docking_station = DockingStation.new
-  it {expect(docking_station).to respond_to(:release_bike)}
+  it {is_expected.to respond_to(:release_bike)}
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+  it "accepts a bike" do
+    is_expected.to respond_to(:dock).with(1).argument
+  end
+  station = DockingStation.new
+  bike = station.release_bike
+  station.dock(bike)
+  it {expect(station.bikes).to eq bike}
 end

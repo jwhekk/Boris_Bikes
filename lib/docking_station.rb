fe7a5c226c0e409bd4fb6a037124c4f3976df10c
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-21 13:28:42 +0100

    raised exception for dock full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,13 @@ class DockingStation
   def release_bike
     if @num_bikes > 0
       @num_bikes -= 1
-      Bike.new
+      @bikes
     else
       raise "No more bikes!"
     end
   end
   def dock(bike)
+    raise "Dockingstation full" if @num_bikes >= 20
     @bikes = bike
     @num_bikes += 1
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,8 @@ require 'docking_station'
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
   it 'releases working bikes' do
-    bike = subject.release_bike
+    
+    bike = Bike.new
     expect(bike).to be_working
   end
   it "accepts a bike" do
@@ -15,5 +16,10 @@ describe DockingStation do
   it {expect(station.bikes).to eq bike}
   it "returns 'No bikes' when out of bikes" do
     expect { 21.times{station.release_bike} }.to raise_error "No more bikes!"
+  end
+  it "returns 'Dockingstation full' when num_bike eq 20" do
+
+
+    expect { 21.times {station.dock(bike) } }.to raise_error "Dockingstation full"
   end
 end

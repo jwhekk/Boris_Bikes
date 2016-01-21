75fd87a817cb665457bdf88d36aa525ab6ac1b26
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-01-21 17:45:12 +0100

    completed challenge 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,12 +7,21 @@ class DockingStation
     20.times { @bikes << Bike.new }
   end
   def release_bike
-    raise "No more bikes!" if @bikes.empty?
+    raise "No more bikes!" if empty?
     @bikes.pop
   end
   def dock(bike)
-    raise "Dockingstation full" if @bikes.count > 20
+    raise "Dockingstation full" if full?
     @bikes << bike
     @bike = bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+  def empty?
+    @bikes.empty?
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,14 +3,14 @@ require 'docking_station'
 describe DockingStation do
   let(:station) { DockingStation.new}
   let!(:bikenumber) {subject.bikes.length}
-  
+
   it "returns '20' when asked for bikes" do
     expect(bikenumber).to eq 20
   end
-  
+
   it {is_expected.to respond_to(:release_bike)}
   it 'releases working bikes' do
-    
+
     bike = Bike.new
     expect(bike).to be_working
   end
@@ -18,12 +18,12 @@ describe DockingStation do
     is_expected.to respond_to(:dock).with(1).argument
   end
 
-  
-  it 'returns bike after you dock it'do 
+
+  it 'returns bike after you dock it'do
     bike = subject.release_bike
-    expect(subject.dock(bike)).to eq bike 
+    expect(subject.dock(bike)).to eq bike
   end
-  
+
   it "returns 'No bikes' when out of bikes" do
     expect { 21.times{subject.release_bike} }.to raise_error "No more bikes!"
   end
@@ -31,4 +31,24 @@ describe DockingStation do
     bike = subject.release_bike
     expect { 21.times {subject.dock(bike) } }.to raise_error "Dockingstation full"
   end
+  # describe "#full?" do
+  #   it { is_expected.to respond_to(:full?)}
+  #   it "returns true when 20" do
+  #     expect(subject.full?).to eq true
+  #   end
+  #   it "returns false when less then 20" do
+  #     subject.release_bike
+  #     expect(subject.full?).to eq false
+  #   end
+  # end
+  # # describe "empty?" do
+  #   it { is_expected.to respond_to(:empty?)}
+  #   it "returns true when 0" do
+  #     20.times { subject.release_bike }
+  #     expect(subject.empty?).to eq true
+  #   end
+  #   it "returns false when > 0" do
+  #     expect(subject.empty?).to eq false
+  #   end
+  # end
 end
